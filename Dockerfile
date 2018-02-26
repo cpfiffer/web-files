e90d6791ba2c77d6d3b262b833dd2efc6fd24138
Cameron Pfiffer <cpfiffer@gmail.com>
2018-02-25 19:29:41 -0800

    New build files.

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM kyma/docker-nginx
-COPY public/ /var/www
-CMD 'nginx'
-EXPOSE 80
-EXPOSE 443
--- themes/minimo/src/scripts/main.js
+++
@@ -1,5 +0,0 @@
-import '../stylesheets/style'
-
-import docReady from 'es6-docready'
-
-docReady(()=>console.log('asd'))
